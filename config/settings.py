2857b8910addb4d83de2fc6e9efdb636ff8a63df
Rohullah Sadat <rohullahsadat1997@gmail.com>
2026-09-20 03:16:51 +0330

    Add database URL dependency

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
